210de188e03a16da748a6fb2e11d334ba36d3b76
Jose Carlos <jesteban@ces.vocento.com>
2020-04-11 19:45:31 +0200

    add Comment Form to twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -54,6 +56,10 @@ class ConferenceController extends AbstractController
         CommentRepository $commentRepository,
         ConferenceRepository $conferenceRepository
     ) {
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class,$comment);
+
         $offset = max(0, $request->query->getInt('offset',0));
         $paginator = $commentRepository->getCommentPaginator($conference,$offset);
         return new Response($this->twig->render('conference/show.html.twig',[
@@ -62,6 +68,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset -  CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView()
         ]));
     }
 }
