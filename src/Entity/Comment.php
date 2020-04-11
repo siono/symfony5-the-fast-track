dfcc3854b1a428d7319e97d6797a84178037e415
Jose Carlos <jesteban@ces.vocento.com>
2020-04-11 12:55:15 +0200

    add createAt value automatic

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Comment
 {
@@ -86,6 +87,14 @@ class Comment
         $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreateAtValue()
+    {
+        $this->createdAt = new \DateTime();
     }
 
     public function getCreatedAt(): ?\DateTimeInterface
