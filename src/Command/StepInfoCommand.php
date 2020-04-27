50fed5e4d122eab2e65627afb5c19ed732cbaae7
Jose Carlos <jesteban@ces.vocento.com>
2020-04-27 18:00:42 +0200

    add command app:step:info

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class StepInfoCommand extends Command
+{
+    protected static $defaultName = 'app:step:info';
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at','HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+            return $process->getOutput();
+        });
+
+        $output->writeln($step);
+        return 0;
+    }
+}
